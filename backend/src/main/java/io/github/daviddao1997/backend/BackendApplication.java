d065dbf138a941e4b58aec90f20c29319a1458d8
JustCallMeTired <127931419+DavidDao1997@users.noreply.github.com>
2026-09-25 12:19:47 +0200

    Add Backend

---
+++ backend/src/main/java/io/github/daviddao1997/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.daviddao1997.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/daviddao1997/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daviddao1997.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
